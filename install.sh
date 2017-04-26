d8d785b116219d5ced783b2e37cbfe4e74eba6b7
Steve Brooks <steve.c.brooks@gmail.com>
2017-04-25 21:22:56 -0400

    fixed install paths

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir ./.vim/bundle
-git clone https://github.com/gmarik/vundle.git ./.vim/bundle/vundle.vim
-ln -s ./.vim ~/.vim
-ln -s ./.vimrc ~/.vimrc
+mkdir ~/dotfiles/.vim/bundle
+git clone https://github.com/gmarik/vundle.git ~/dotfiles/.vim/bundle/vundle.vim
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+
 
